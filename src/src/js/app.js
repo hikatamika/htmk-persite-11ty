8e6baf93e65275cc5ad9b491c8d94dffbe4c61e0
hikatamika <36945134+hikatamika@users.noreply.github.com>
2025-06-28 05:38:08 -0700

    Switching to modal-style navigation

--- src/src/js/app.js
+++ src/src/js/app.js
@@ -7,18 +7,55 @@ for (let i = 0; i < anchorLinks.length; i += 1) {
   }
 }
 
-// Mobile Nav Button
+// Modal Stuff
 {
-  var mobileNavBtn = document.getElementById('mobileNavKnob');
-  var mobileNav = document.getElementById('mobileNavDrawer');
-  var mobileNavLst = document.getElementById('mobileNavList');
+  // Modal Popups
+  {
+    // open modal by id
+    function openModal(id, id2) {
+      document.getElementById(id).classList.add('open');
+      // document.body.classList.add('nav-modal-open');
+      window.setTimeout(function() {
+        document.getElementById(id2).classList.add('fade-in');
+      }, 50)
+      // document.getElementById(id2).classList.remove('fade-out');
+    }
 
-  mobileNavBtn.addEventListener('click', function() {
-    mobileNav.classList.toggle('mobile-nav-open');
-    if (mobileNav.style.maxHeight){
-      mobileNav.style.maxHeight = null;
-    } else {
-      mobileNav.style.maxHeight = mobileNav.scrollHeight + "px";
-    } 
-  }, false);
+    // close currently open modal (id2)
+    function closeModal(id, id2) {
+      // document.body.classList.remove('nav-modal-open');
+      // document.getElementById(id2).classList.add('fade-out');
+      document.getElementById(id2).classList.remove('fade-in');
+      window.setTimeout(function() {
+        document.querySelector('.nav-modal.open').classList.remove('open');
+      }, 350);
+    }
+
+    window.addEventListener('load', function() {
+      // close modals on background click
+      document.addEventListener('click', event => {
+          if (event.target.classList.contains('nav-modal')) {
+              closeModal();
+          }
+      });
+    });
+  }
+
+  // Show Button
+  {
+    // show button by id
+    function toggleButton(id, id2) {
+      document.getElementById(id).classList.add('button-show');
+      document.getElementById(id).classList.remove('button-hide');
+      document.getElementById(id2).classList.add('button-hide');
+      document.getElementById(id2).classList.remove('button-show');
+    }
+  }
+
+  // Click Events
+  let mobileCloseButton = document.querySelector('button#mobileNavClose');
+  let mobileOpenButton = document.querySelector('button#mobileNavButton');
+
+  mobileCloseButton.onclick = function() { closeModal('navModal', 'navModalBody');toggleButton('sidebarMobileButton', 'mobileNavClose');};
+  mobileOpenButton.onclick = function() { openModal('navModal', 'navModalBody');toggleButton('mobileNavClose', 'sidebarMobileButton');};
 }
